914ae69563512f23187a9f6b9fe69a64e82d12f8
OmJRamdas <93028735+OmJRamdas@users.noreply.github.com>
2025-01-26 15:52:46 +0800

    Followed instructions required
    Changed naming convention from Duke to Clanker
    Updated output and ReadME

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Clanker User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Clanker.java
@@ -0,0 +1,17 @@
+public class Clanker {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        String logo = " _____ _             _\n"
+                + "/  __ \\ |           | |\n"
+                + "| /  \\/ | __ _ _ __ | | _____ _ __ \n"
+                + "| |   | |/ _` | '_ \\| |/ / _ \\ '__| \n"
+                + "| \\__/\\ | (_| | | | |   <  __/ | \n"
+                + " \\____/_|\\__,_|_| |_|_|\\_\\___|_| \n";
+        System.out.println("Hello I'm\n" + logo + "\n"+ "What Can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye! Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
