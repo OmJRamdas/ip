44f9d62cc260203a8be6e0889b2ab2298bafa1ab
OmJRamdas <93028735+OmJRamdas@users.noreply.github.com>
2025-02-04 21:27:25 +0800

    Refactored Code and added chatLoop

--- src/main/java/Clanker.java
+++ src/main/java/Clanker.java
@@ -1,17 +1,57 @@
-public class Clanker {
-    public static void main(String[] args) {
-        String line = "____________________________________________________________";
+import java.util.Scanner;
 
-        System.out.println(line);
-        String logo = " _____ _             _\n"
+public class Clanker {
+
+        private static final String LINE = "____________________________________________________________";
+        private static final String LOGO = " _____ _             _\n"
                 + "/  __ \\ |           | |\n"
                 + "| /  \\/ | __ _ _ __ | | _____ _ __ \n"
                 + "| |   | |/ _` | '_ \\| |/ / _ \\ '__| \n"
                 + "| \\__/\\ | (_| | | | |   <  __/ | \n"
                 + " \\____/_|\\__,_|_| |_|_|\\_\\___|_| \n";
-        System.out.println("Hello I'm\n" + logo + "\n"+ "What Can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye! Hope to see you again soon!");
-        System.out.println(line);
+
+
+        private void greet() {
+            System.out.println(LINE);
+            System.out.println("Hello I'm\n" + LOGO + "\n"+ "What Can I do for you?");
+            System.out.println(LINE);
+        }
+
+        private void goodbye() {
+            System.out.println("Bye! Hope to see you again soon!");
+            System.out.println(LINE);
+        }
+
+        private String getUserInput() {
+            Scanner scanner = new Scanner(System.in);
+            return scanner.nextLine();
+        }
+
+        private void echoUserCommand(String command) {
+            System.out.println(LINE);
+            System.out.println(command);
+            System.out.println(LINE);
+        }
+
+        private void chatLoop() {
+            boolean isRunning= true;
+            while (isRunning) {
+                String userCommand = getUserInput();
+                if (userCommand.equals("bye")) {
+                    isRunning = false;
+                }
+                echoUserCommand(userCommand);
+            }
+        }
+
+        public void chatBotProgram() {
+            greet();
+            chatLoop();
+            goodbye();
+        }
+
+        public static void main(String[] args) {
+            Clanker chatbot = new Clanker();
+            chatbot.chatBotProgram();
+        }
     }
-}
