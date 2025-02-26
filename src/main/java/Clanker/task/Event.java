8852b65fc368be23c293906890e123716bf0b48b
OmJRamdas <93028735+OmJRamdas@users.noreply.github.com>
2025-02-26 18:32:37 +0800

    Date time added to event and deadline

--- src/main/java/Clanker/task/Event.java
+++ src/main/java/Clanker/task/Event.java
@@ -1,13 +1,15 @@
 package Clanker.task;
 
+import Clanker.parser.dateTimeParser;
+
 public class Event extends Task {
     protected String startTime;
     protected String endTime;
 
     public Event(String name, String startTime, String endTime) {
         super(name);
-        this.startTime = startTime;
-        this.endTime = endTime;
+        this.startTime = dateTimeParser.parseDateTime(startTime);
+        this.endTime = dateTimeParser.parseDateTime(endTime);
     }
 
     @Override
